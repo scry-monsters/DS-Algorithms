2151df0ae15b9866d5d4203e867f62fb2486076d
Aziz Kurbanov <krbnv.z@gmail.com>
2022-01-29 22:57:05 +0300

    bubble

--- bubble__sort/index.js
+++ bubble__sort/index.js
@@ -2,7 +2,7 @@ let arr = [1,6,3,4,9,5];
 
 let holder = (arr) => {
     for(let i = 0; i < arr.length; i++){
-        for(let j = 1; j < arr.length - 1; j++){
+        for(let j = 1; j < arr.length - i; j++){ //removing the need to check the already sorted numbers on new iterations optimizes the code(even though it's still O(n^2))
             if(arr[j] < arr[j - 1]){
                 let numHolder = arr[j];
                 arr[j] = arr[j - 1];
