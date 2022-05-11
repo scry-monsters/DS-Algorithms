be0c5188c408c56ae3033618f688920abe0566ba
Aziz Kurbanov <krbnv.z@gmail.com>
2022-05-11 14:11:07 +0600

    *

--- binary__search/index.js
+++ binary__search/index.js
@@ -1,23 +1,24 @@
 function binarySearch(arr, value) {
-    let min = 0;
-    let max = arr.length - 1;
-    while(min <= max){
-    console.log("ye")
-    const mid = min + Math.floor((max - min) / 2);
-    let guess = arr[mid];
-    if(guess === value){
-      return mid;
-    }
-    if(value < guess){
-      max = mid - 1
-    }
-    else {
-      min = mid + 1
-    }
-    }
-    return "Value Not Found"
+  let counter = 0;
+   let smallest =  0;
+   let biggest  = arr.length - 1;
+   while(smallest < biggest && value < arr[biggest]){
+     counter++;
+     let middleIndex = Math.floor((smallest + biggest) / 2);
+     let guess = arr[middleIndex];
+     if(guess === value){
+       return counter;
+     }
+     if(guess < value){
+       smallest = middleIndex + 1;
+     }
+     else{
+       biggest = middleIndex - 1;
+     }
+   }
+   return counter;
   }
 
 let testArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-binarySearch(testArr, 8);
+console.log(binarySearch(testArr, 15));
---
+++ linked_list/index.js
@@ -0,0 +1,108 @@
+class Node {
+	constructor(data, next = null) {
+		this.data = data;
+		this.next = next;
+	}
+}
+
+class LinkedList {
+	constructor(head, size = 0) {
+		this.head = head;
+		this.size = size;
+	}
+
+	insertFirst(data) {
+		this.head = new Node(data, this.head);
+		this.size++;
+	}
+
+	insertLast(data) {
+		let node = new Node(data);
+		let current;
+		if (!this.head) {
+			this.head = node;
+			this.size++;
+		} else {
+			current = this.head;
+			while (current.next) {
+				current = current.next;
+			}
+			current.next = node;
+		}
+		this.size++;
+	}
+
+	insertAt(data, index){
+        if(index > 0 && index > this.size){
+            return;
+        }
+        if(index === 0){
+            this.head = new Node(data, this.head);
+            this.size++;
+            return;
+        } 
+
+        const node = new Node(data);
+        let current, previous;
+
+        current = this.head;
+        let count = 0;
+
+        while(count < index){
+            previous = current;
+            count++;
+            current = current.next;
+        }
+
+        node.next = current;
+        previous.next = node;
+
+        this.size++;
+    }
+
+    removeElem(index){
+        if(index < 0 || index > this.size){
+            return;
+        }
+        let current = this.head;
+        let previous;
+        let count = 0;
+        if(index === 0){
+            this.head = current.next;
+        }
+        else{
+            while(count  < index){
+                count++;
+                previous = current;
+                current = current.next;
+            }
+            previous.next = current.next
+        }
+        this.size--;
+    }
+
+    printList(){
+        while(this.head){
+            console.log(this.head);
+            this.head = this.head.next;
+        }
+    }
+
+    reverseLL(){
+        let current;
+        let previous = null;
+        let next = null;
+        while(current !== null){
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next; 
+        }
+        return previous;
+    }
+}
+
+const ll = new LinkedList();
+
+
+
