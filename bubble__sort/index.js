35b8dc6bcde4a675ee67f9d88f34f3c325346087
Aziz Kurbanov <krbnv.z@gmail.com>
2022-07-04 22:43:45 +0600

    *

--- bubble__sort/index.js
+++ bubble__sort/index.js
@@ -1,18 +1,17 @@
-let testArr = [1,6,3,4,9,5];
+let testArr = [0, 1, 6, 3, 4, 9, 5, 0];
 
 let bubbleSort = (arr) => {
-    for(let i = 0; i < arr.length; i++){
-        for(let j = 1; j < arr.length - i; j++){ //removing the need to check the already sorted numbers on new iterations optimizes the code(even though it's still O(n^2))
-            if(arr[j] < arr[j - 1]){
-                let numHolder = arr[j];
-                arr[j] = arr[j - 1];
-                arr[j - 1] = numHolder;
-            }
-        }
-    }
-    return arr
-}
+	let newArr = [...arr];
+	for (let i = 0; i < newArr.length; i++) {
+		for (let j = 1; j < newArr.length; j++) {
+			if (newArr[j] > newArr[j + 1]) {
+				let holder = newArr[j + 1];
+				newArr[j + 1] = newArr[j];
+				newArr[j] = holder;
+			}
+		}
+	}
+	return newArr;
+};
 
-console.log(bubbleSort(testArr))
-
-
+console.log(bubbleSort(testArr));
