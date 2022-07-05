d1eab91f4e01f39765f657e986fbc806a9b21b0c
Aziz Kurbanov <krbnv.z@gmail.com>
2022-07-06 02:31:31 +0600

    *

--- search_insert_position/index.js
+++ search_insert_position/index.js
@@ -1,14 +1,14 @@
-var searchInsert = function(nums, target) {
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] === target){
-            return i; // if found then just give the index
-        }
-        if(nums[i] >= target){
-            return i; //we will find the first number that is bigger than the target and give the position before it, so if you want to find 2 and it doesn't exist in the list, then we have to find the first number that is bigger than the target value(which is 3) and give the position before it so 1 is the answer
-        }
-    }
-    return nums.length //if bigger than the last number, it's safe to assume that it will the place of the last element so nums.length will be the index of the new element introduced to the list 
+function searchInsert(nums, target) {
+   let arr = [...nums];
+   for(let i = 0; i < arr.length; i++){
+       if(arr[i] === target){
+           return i;
+       }
+       if(arr[i] > target){
+           return i;
+       }
+   }
+   return nums.length;
 };
 
-
-console.log(searchInsert([1,3,4,5], 2))
+console.log(searchInsert([1,3,5,6], 2));
