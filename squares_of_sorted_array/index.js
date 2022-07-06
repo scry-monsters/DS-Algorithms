7cbc6d9918f8ea4dcb3dd5fe8192195a7b373c8e
Aziz Kurbanov <krbnv.z@gmail.com>
2022-07-06 22:29:59 +0600

    *

--- squares_of_sorted_array/index.js
+++ squares_of_sorted_array/index.js
@@ -1,21 +1,17 @@
-var sortedSquares = function(nums) {
-    let startPointer = 0
-    let endPointer = nums.length - 1
-    let res = []
-    
-    while (startPointer <= endPointer) {
-    console.log(res)
-
-        if(Math.abs(nums[startPointer]) > Math.abs(nums[endPointer])) {
-            res.unshift(nums[startPointer] ** 2)
-            startPointer++
-        } else {
-            res.unshift(nums[endPointer] ** 2)
-            endPointer--
-        }
-    }
-    return res.sort((a, b) => a - b)
+var sortedSquares = function (nums) {
+	let first = 0;
+	let last = nums.length - 1;
+	let holder = [];
+	while (first <= last) {
+		if (Math.abs(nums[first] < Math.abs(nums[last]))) {
+			holder.unshift(nums[last] ** 2);
+			last--;
+		} else {
+			holder.unshift(nums[last] ** 2);
+			last--;
+		}
+	}
+	return holder.sort((a, b) => a - b);
 };
 
-
-console.log(sortedSquares([-7,-3,6,3,11]))
+console.log(sortedSquares([-7, -3, 6, 3, 11]));
