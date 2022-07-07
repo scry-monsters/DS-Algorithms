c2c4be4f00bfe3504fcb33f916e79d92ed39cf25
Aziz Kurbanov <krbnv.z@gmail.com>
2022-07-08 01:45:37 +0600

    *

--- Two Sum II - Input Array Is Sorted/index.js
+++ Two Sum II - Input Array Is Sorted/index.js
@@ -1,15 +1,21 @@
-var twoSum = function (numbers, target) {
-	if (numbers.length < 2) return;
-	let start = 0;
-	for (let i = 1; i < numbers.length; i++) {
-        for(let j = 1; j < numbers.length; j++){
-            if (numbers[start] + numbers[j] === target) {
-                return [start + 1, j + 1];
-            } else {
-                start++;
-            }
-        }
-        }
-};
+var twoSum = function(numbers, target) {
+    let start = 0;
+      let end = numbers.length - 1;
+      while(start <= end){
+          let sum = numbers[start] + numbers[end];
+          if(sum === target){
+              return [start + 1, end + 1];
+          }
+          else if(sum < target){
+              start++;
+          }
+          else{
+              end--;
+          }
+      }
+      return [-1, -1]
+  };
+  
 
-console.log(twoSum([2, 3, 4], 6));
+
+console.log(twoSum([1,2,3,4,5], 7))
