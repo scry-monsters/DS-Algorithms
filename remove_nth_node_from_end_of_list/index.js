149bd947f7bba1337d2ac0ae68a8b6c9d39f436f
Aziz Kurbanov <krbnv.z@gmail.com>
2022-07-12 22:06:25 +0600

    *

--- remove_nth_node_from_end_of_list/index.js
+++ remove_nth_node_from_end_of_list/index.js
@@ -13,24 +13,7 @@ class LinkedList{
     }
 
     removeNth(elem){
-        if(elem < 0 || elem > this.size){
-            return;
-        }
-        let current = this.head;
-        let previous;
-        let count = 0;
-        if(elem === 0){
-            this.head = current.next;
-        }
-        else{
-            while(count < elem){
-                count++;
-                previous = current;
-                current = current.next;
-            }
-            previous.next = current.next;
-        }
-        this.size--;
+        
     }
 }
 
