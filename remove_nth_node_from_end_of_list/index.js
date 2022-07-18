b5be91114aa01874535c5840665b2b8b2b365fb3
Aziz Kurbanov <krbnv.z@gmail.com>
2022-07-18 22:33:55 +0600

    *

--- remove_nth_node_from_end_of_list/index.js
+++ remove_nth_node_from_end_of_list/index.js
@@ -11,9 +11,37 @@ class LinkedList{
         this.size = size;
     }
 
+    addElemToBegin(data){
+        this.head = new Node(data, this.head);
+        this.size++;
+    }
+
 
     removeElem(index){
+        if(index < 0 || index > this.size){
+            return;
+        }
         let current = this.head;
+        let previous;
+        let count = 0;
+        if(index === 0){
+            this.head = current.next;
+        }
+        else{
+            while(count < index){
+                count++;
+                previous = current;
+                current = current.next;
+            }
+            previous.next = current.next;
+        }
+        this.size--;
     }
 }
 
+let ll = new LinkedList();
+ll.addElemToBegin(300);
+ll.addElemToBegin(200);
+ll.addElemToBegin(100);
+ll.removeElem(1);
+console.log(ll.size);
