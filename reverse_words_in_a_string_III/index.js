280853582fcccd5fbeffe62e5d9bfd7ca41c572f
Aziz Kurbanov <krbnv.z@gmail.com>
2022-07-25 21:48:40 +0600

    *

--- reverse_words_in_a_string_III/index.js
+++ reverse_words_in_a_string_III/index.js
@@ -6,13 +6,44 @@ Output: "s'teL ekat edoCteeL tsetnoc"
 */
 
 var reverseWords = function(s) {
-  let arr = s.split(" ");
-  let holder = "";
-     for(let i = 0; i < arr.length; i++){
-       holder += arr[i].split("").reverse().join("") + " ";
-     }
-     return holder.trim();
-};
+     let holder = [];
+    let splitty = s.split(" ");
+for(let i = 0; i < splitty.length; i++){
+    holder.push(splitty[i].split("").reverse().join(""));
+}
+    return holder.join(" ");
+}
 
 
 console.log(reverseWords("doG gniD"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let arr = s.split(" ");
+//   let holder = "";
+//      for(let i = 0; i < arr.length; i++){
+//        holder += arr[i].split("").reverse().join("") + " ";
+//      }
+//      return holder.trim();
