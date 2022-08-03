6c7985788572a7594b1b93a5959d31b2820da262
Aziz Kurbanov <krbnv.z@gmail.com>
2022-08-03 18:07:45 +0600

    beginner mistakeeeeee

--- bubble__sort/index.js
+++ bubble__sort/index.js
@@ -1,9 +1,9 @@
-let testArr = [0, 1, 6, 3, 4, 9, 5, 0];
+let testArr = [9, 1, 6, 3, 4, 9, 5, 0];
 
 let bubbleSort = (arr) => {
 	let newArr = [...arr];
 	for (let i = 0; i < newArr.length; i++) {
-		for (let j = 1; j < newArr.length; j++) {
+		for (let j = 0; j < newArr.length; j++) {
 			if (newArr[j] > newArr[j + 1]) {
 				let holder = newArr[j + 1];
 				newArr[j + 1] = newArr[j];
