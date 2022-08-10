3ec7fa711d2a4352f5dc3679a20b7c5dabc9ea63
Aziz Kurbanov <krbnv.z@gmail.com>
2022-08-10 14:27:14 +0600

    *

--- merge__sort/index.js
+++ merge__sort/index.js
@@ -1,39 +1,38 @@
-function merge(leftArr, rightArr){
+function sortArr(leftArr, rightArr){
     let arr = [];
     let leftIndex = 0;
-    let rightIndex =  0;
-
+    let rightIndex = 0;
     while(leftIndex < leftArr.length && rightIndex < rightArr.length){
-        const leftEl = leftArr[leftIndex];
-        const rightEl = rightArr[rightIndex];
-        if(leftEl < rightEl){
-            arr.push(leftEl)
-            leftIndex++;
-        }
-        else{
-            arr.push(rightEl);
-            rightIndex++;
-        }
-
+      let leftNum = leftArr[leftIndex];
+      let rightNum = rightArr[rightIndex];
+      if(leftNum < rightNum){
+        arr.push(leftNum);
+        leftIndex++;
+      }
+      else{
+        arr.push(rightNum);
+        rightIndex++;
+      }
     }
-    return [...arr, ...leftArr.slice(leftIndex), ...rightArr.slice(rightIndex)]
-  }
-  
-  function mergeSort(arr) {
+    return [...arr, ...leftArr.slice(leftIndex), ...rightArr.slice(rightIndex)];
+    }
+    
+    function mergeSort(arr) {
       if(arr.length <= 1){
-          return arr;
+        return arr;
       }
       let middleIndex = Math.floor(arr.length / 2);
       let leftArr = arr.slice(0, middleIndex);
       let rightArr = arr.slice(middleIndex);
-
-      return merge(
-          mergeSort(leftArr),
-          mergeSort(rightArr)
-      )
-
-  }
-  
+    
+      // Only change code below this line
+    
+      return sortArr(
+        mergeSort(leftArr),
+        mergeSort(rightArr)
+      );
+      // Only change code above this line
+    }
   
   
   console.log(mergeSort([1,4,2,8,5,6,7,3,2]))
