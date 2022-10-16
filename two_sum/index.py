26331afa402ab412dfaa51481466b0745e012f70
Aziz Kurbanov <krbnv.z@gmail.com>
2022-10-16 23:31:42 +0300

    *

--- two_sum/index.py
+++ two_sum/index.py
@@ -1,9 +1,20 @@
+# first solution
+
 class Solution(object):
-    def twoSum(nums, target):
+    def twoSum(self, nums, target):
         for i in range(0, len(nums)):
             for j in range(i+1, len(nums)):
                 holder = nums[i] + nums[j]
                 if holder == target:
                     return [i, j]
 
-    print(twoSum([2, 7, 4, 5, 6], 9))
+
+# better solution
+
+class Solution(object):
+    def twoSum(nums, target):
+        dictionary = {}
+        for index, elem in enumerate(nums):
+            if target - elem in dictionary:
+                return dictionary[target - elem], index
+            dictionary[elem] = index
