d58fccbb1d8e1c20501485052c0deae2d37c7eb8
Aziz Kurbanov <krbnv.z@gmail.com>
2023-01-01 23:55:00 +0600

    *

--- test/index.js
+++
@@ -1,7 +0,0 @@
-let arr = [1,2,3,3,4,5,5,6,7];
-for(let i = 1; i < arr.length; i++){
-    if(arr[i] === arr[i - 1]){
-        arr.splice(i,1);
-    }
-}
-console.log(arr)
--- test/index.py
+++ test/index.py
@@ -1,12 +1,3 @@
-
-def removeDuplicates(nums: list[int]) -> int:
-    k = 1
-    for i in range(1, len(nums)):
-        if nums[i-1] != nums[i]:
-            nums[k] = nums[i]
-            k += 1
-            print(nums)
-    return k
-
-
-print(removeDuplicates([1, 2, 2, 3, 4, 4]))
+new_array = []
+dictionary = dict()
+print(dictionary)
